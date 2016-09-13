2b34d7f14673106c550c8b9fc413498efd273039
Vishu Mallela <vishumallela@icloud.com>
2016-09-13 09:33:15 +0530

    Created hello-world.py
    Added the code to print a simple hello-world message.

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+#To make a simple hello-world program
+
+print("Hello World!")
+print("I am _________") '''Your name here
+print("I love _________") '''Your favorite hobby here
+print("Do check out my profile on ____ : _______") '''Your website and username here
+print("Bye For Now!!!")
